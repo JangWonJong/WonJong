4682907199fd0402871dd1b76573b1e3b8eb4907
JangWonJong <jwj9922@naver.com>
2022-01-26 17:56:40 +0900

    17:56 grade 1단계 작업

---
+++ java/com/example/sample/abc/AbcAPP.java
@@ -0,0 +1,20 @@
+package com.example.sample.abc;
+
+/**
+ * packageName: com.example.sample
+ * fileName   : AbcAPP
+ * author     : Jangwonjong
+ * date       : 2022-01-26
+ * desc       :
+ * ======================================
+ * DATE          AUTHOR            NOTE
+ * ======================================
+ * 2022-01-26     Jangwonjong       최초 생성
+ */
+public class AbcAPP {
+    public String getAbc(){
+        String abc = String.format("");
+        return abc;
+
+    }
+}
--- java/com/example/sample/line/LineDemo.java
+++ java/com/example/sample/line/LineDemo.java
@@ -1,13 +1,23 @@
 package com.example.sample.line;
 
+import com.example.sample.calc.CalcApp;
+
 import java.util.Scanner;
 
 public class LineDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LineApp lineApp = new LineApp();
-        System.out.println();
+        System.out.println(LineApp.LINE_TITLE);
+        System.out.println("첫번째 숫자 : ");
+        int num1 = scanner.nextInt();
+        System.out.println("연산자 + - * / : ");
+        String opcode = scanner.next();
+        System.out.println("두번째 숫자 : ");
+        int num2 = scanner.nextInt();
+        String result = lineApp.lin(num1, opcode, num2);
+        System.out.println("연산결과 : "+ result);
+    }
 
     }
 
-}

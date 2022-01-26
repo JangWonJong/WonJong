6f6209e274179042e108274805588485ea19f3c8
JangWonJong <jwj9922@naver.com>
2022-01-26 16:20:34 +0900

    16:20 3항연산자

--- java/com/example/sample/login/LoginApp.java
+++ java/com/example/sample/login/LoginApp.java
@@ -22,18 +22,21 @@ public class LoginApp {
     this.id = id;
     this.pw = pw;
     this.name = name;
-
     String res = "";
+
     /*if(pw.equals(PASSWORD)){
         res = String.format(" %s 님의 비번 %s 가 맞습니다. 로그인 성공 ", this.name, this.pw);
     }else{
         res = String.format(" %s 의 ID 는 맞고, 비번 %s 가 틀립니다. 로그인 실패 ", this.id, this.pw);
     }*/
 
-    switch (pw){
+    /*switch (pw){
         case "abc" : res = String.format(" %s 님의 비번 %s 가 맞습니다. 로그인 성공 ", this.name, this.pw);break;
         default: res = String.format(" %s 의 ID 는 맞고, 비번 %s 가 틀립니다. 로그인 실패 ", this.id, this.pw); break;
-    }
+    }*/
+
+    res = (pw.equals(PASSWORD)) ? String.format(" %s 님의 비번 %s 가 맞습니다. 로그인 성공 ", this.name, this.pw)
+            : String.format(" %s 의 ID 는 맞고, 비번 %s 가 틀립니다. 로그인 실패 ", this.id, this.pw);
 
 
     return res;

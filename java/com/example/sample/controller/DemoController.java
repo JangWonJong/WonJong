d5676b4c1793e0add4596efa6cd020539e0064f8
JangWonJong <jwj9922@naver.com>
2022-01-27 15:17:45 +0900

    15:17 DemoController 추가

---
+++ java/com/example/sample/controller/DemoController.java
@@ -0,0 +1,38 @@
+package com.example.sample.controller;
+
+import java.util.Scanner;
+
+/**
+ * packageName: com.example.sample.controller
+ * fileName   : DemoController
+ * author     : Jangwonjong
+ * date       : 2022-01-27
+ * desc       :
+ * ======================================
+ * DATE          AUTHOR            NOTE
+ * ======================================
+ * 2022-01-27     Jangwonjong       최초 생성
+ */
+public class DemoController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0; i<2; i++){
+            System.out.println("메뉴 선택");
+            String menu =  "1.BMI 2.CALC 3.SEARCH 4.GRADE 5.LOGIN";
+            System.out.println(menu);
+            String select = scanner.next();
+            String res = "";
+            switch (select){
+                case "1": res = "BMI" ; break;
+                case "2": res = "CALC"; break;
+                case "3": res = "SEARCH"; break;
+                case "4": res = "GRADE" ; break;
+                case "5": res = "LOGIN" ; break;
+                default : res = "Invalid MENU"  ; break;
+            }
+            System.out.println(res);
+
+        }
+
+    }
+}

fd29ce2b339a79d82be9083c6ff787a5cd81a75c
JangWonJong <jwj9922@naver.com>
2022-02-07 13:26:31 +0900

    13:26

--- java/com/example/sample/controller/TempController.java
+++ java/com/example/sample/controller/TempController.java
@@ -19,60 +19,55 @@ public class TempController {
                         "BFS",          "Stack",        "Hash",        "Bin",     "DP",
                         "DFS",          "Queue",        "Sort",        "Greedy",  "Graph",
                         "Brute Force",  "Heap"};
+        Scanner scanner = new Scanner(System.in);
+        String menu = "Q1, Q2, Q3, Q4";
+        System.out.println(menu);
+        String res = "";
+        while (true){
+            switch (scanner.next()){
+                case "0" :
+                    System.out.println("시스템을 종료합니다"); return;
+                case "1":
+                    System.out.println("Q1. 팀별 과제를 출력하세요 ");
+                    quiz1(arr);
+                    break;
+                case "2":
+                    System.out.println("Q2. 팀장이 맡은 과제만 출력하세요. 예) 강민, DP, Graph");
+                    quiz2(arr);
+                    break;
+                case "3":
+                    System.out.println("Q3. 큐를 담당한 사람을 출력하세요.");
+                    quiz3(arr);
+                    break;
+                case "4":
+                    System.out.println("Q4. 팀원별 과제 수를 출력하세요.");
+                    quiz4(arr);
+                    break;
 
-        System.out.println("Q1. 팀별 과제를 출력하세요 ");
-        String s = "";
-        for (int i = 0; i < arr.length; i++) {
-            if (i%5==0) { s += "\n";
-            s += i + " : " + arr[i] + "\t";
-        }
-        System.out.println(s);
-        s = "";
-        System.out.println("Q2. 팀장이 맡은 과제만 출력하세요 ");
-        String l = "강민";
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(l)) {
-                for (int j = 0; j < 4; j++) {
-                    s += arr[i + j * 5] + ","; }
-                if (i == 17) {break; }
+                default: res = "Invalid Menu"; break;
             }
+            System.out.println(res);
         }
-        System.out.println(s);
-        s = "";
-        String j = "Queue";
-        System.out.println("Q3. 큐를 담당한 사람을 출력하세요.");
-        /*s = arr[11] + "를 담당한 사람 : " + arr[1];*/
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(j)) {
-                s += arr[i - 10];
-            }
+
+
         }
-        System.out.println(s);
-        s = "";
-        /*입력한 과목의 인덱스를 추출하세요.");
-        //5, 10, 15 -> 0
-        //6, 11, 16 -> 1
-        //7, 12, 17 -> 2
-        }*/
 
-
-
-        System.out.println("Q4. 팀원별 과제 수를 출력하세요.");
+    private static void quiz4(String[] arr) {
         int count = 0;
         String countManager = "";
-        /*countManager += arr[0]+"(3개),";
+        countManager += arr[0]+"(3개),";
         countManager += arr[1]+"(3개),";
         countManager += arr[2]+"(2개),";
         countManager += arr[3]+"(2개),";
-        countManager += arr[4]+"(2개),";*/
+        countManager += arr[4]+"(2개),";
         int count0 = 0;
         int count1 = 0;
         int count2 = 0;
         int count3 = 0;
         int count4 = 0;
         int[] intArr = new int[5];
-        for (int i=0; i< arr.length; i++){
-            if(arr[i].equals(arr[5])){
+        for (int i = 0; i< arr.length; i++){
+            if(arr[i].equals(arr[i%5])){
                 count0++;
             }
             if(arr[i].equals(arr[6])){
@@ -93,8 +88,49 @@ public class TempController {
         System.out.println(arr[2]+"의 카운트는 "+count2);
         System.out.println(arr[3]+"의 카운트는 "+count3);
         System.out.println(arr[4]+"의 카운트는 "+count4);
-
     }
-}}
+
+    private static void quiz3(String[] arr) {
+        String s = "";
+        String j = "Queue";
+
+        /*s = arr[11] + "를 담당한 사람 : " + arr[1];*/
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(j)) {
+                s = arr[i] + "의 담당자는 " + arr[i%5] ;
+            }
+        }
+        System.out.println(s);
+    }
+
+    private static void quiz2(String[] arr) {
+        String s = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("강민")) {
+                for (int j = 0; j < 3; j++) {
+                    s += arr[i + j * 5] + ","; }
+                if (i == 17) {break; }
+            }
+        }
+        System.out.println(s);
+    }
+
+
+    private static void quiz1(String[] arr) {
+        String s = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (i%5==0) { s += "\n";
+                s += i + " : " + arr[i] + "\t";
+            }
+        }
+        System.out.println(s);
+    }
 }
+
+
+/*입력한 과목의 인덱스를 추출하세요.");
+        //5, 10, 15 -> 0
+        //6, 11, 16 -> 1
+        //7, 12, 17 -> 2
+        }*/
 
